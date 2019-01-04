45518033046f757c8dae9ed3248e1391219764d4
Dreamwalker <aristojeff@gmail.com>
2019-01-05 00:59:55 +0900

    졸려

--- app/src/main/java/com/dreamwalker/myapplication103/activity/UserRegisterActivity.kt
+++ app/src/main/java/com/dreamwalker/myapplication103/activity/UserRegisterActivity.kt
@@ -1,9 +1,14 @@
 package com.dreamwalker.myapplication103.activity
 
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View.OnTouchListener
 import androidx.appcompat.app.AppCompatActivity
 import com.dreamwalker.myapplication103.R
 import com.dreamwalker.myapplication103.intent.AppConst.NFC_TAG_ID_INTENT
+import kotlinx.android.synthetic.main.activity_user_register.*
+
+
 
 class UserRegisterActivity : AppCompatActivity() {
 
@@ -11,7 +16,25 @@ class UserRegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_register)
         val tagId = intent.getStringExtra(NFC_TAG_ID_INTENT)
-        title = tagId
+        title = "TAG Address : " + tagId.toUpperCase()
+
+
+
+        user_sex_tiet.setOnTouchListener(OnTouchListener { v, event ->
+            val DRAWABLE_LEFT = 0
+            val DRAWABLE_TOP = 1
+            val DRAWABLE_RIGHT = 2
+            val DRAWABLE_BOTTOM = 3
+
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (event.rawX >= user_sex_tiet.getRight() - user_sex_tiet.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width()) {
+                    // your action here
+
+                    return@OnTouchListener true
+                }
+            }
+            false
+        })
 
 
     }
