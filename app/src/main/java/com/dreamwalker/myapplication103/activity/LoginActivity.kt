0e9c785fa61aa94b412e0d78ab2b13ed2de99526
Dreamwalker <aristojeff@gmail.com>
2019-01-04 11:17:54 +0900

    ff

---
+++ app/src/main/java/com/dreamwalker/myapplication103/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.dreamwalker.myapplication103.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login2)
+    }
+}
