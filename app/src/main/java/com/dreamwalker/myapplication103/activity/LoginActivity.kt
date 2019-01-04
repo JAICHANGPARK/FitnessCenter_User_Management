2833861af71c970b459b2d6674fc7b06fd10f42b
Dreamwalker <aristojeff@gmail.com>
2019-01-04 11:57:45 +0900

    android x

--- app/src/main/java/com/dreamwalker/myapplication103/activity/LoginActivity.kt
+++ app/src/main/java/com/dreamwalker/myapplication103/activity/LoginActivity.kt
@@ -1,12 +1,47 @@
 package com.dreamwalker.myapplication103.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import androidx.appcompat.app.AppCompatActivity
+import com.dreamwalker.myapplication103.MainActivity
+import com.dreamwalker.myapplication103.R
+import kotlinx.android.synthetic.main.activity_login2.*
+
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login2)
+
+        next_button.setOnClickListener {
+            if (!isPasswordValid(password_edt.text!!)) {
+                password_text_input.error = "error"
+
+            } else {
+                password_text_input.error = null
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        password_edt.setOnKeyListener { _, _, _ ->
+            if (isPasswordValid(password_edt.text!!)) {
+                password_text_input.error = null
+            }
+            false
+        }
+
+        cancel_button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
+
+    private fun isPasswordValid(text: Editable?): Boolean {
+        return text != null && text.length >= 8
+    }
+
 }
