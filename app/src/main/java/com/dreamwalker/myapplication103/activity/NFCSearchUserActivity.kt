c1c62b2d660387c293dbd3f3fff56ecbf072a9ec
Dreamwalker <aristojeff@gmail.com>
2019-01-05 16:33:10 +0900

    nfc

---
+++ app/src/main/java/com/dreamwalker/myapplication103/activity/NFCSearchUserActivity.kt
@@ -0,0 +1,13 @@
+package com.dreamwalker.myapplication103.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dreamwalker.myapplication103.R
+
+class NFCSearchUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nfcsearch_user)
+    }
+}
